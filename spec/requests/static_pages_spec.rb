aa53db39b2ea8286ad6fcfeaa889cde5be776b3a
Stefania Druga <druga.stefania@gmail.com>
2013-05-28 15:28:18 +0200

    capibara gem

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,12 +1,7 @@
-require 'spec_helper'
+describe "Home page" do
 
-describe "Static pages" do
-
-  describe "Home page" do
-
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      page.should have_content('Sample App')
-    end
+  it "should have the content 'Sample App'" do
+    visit '/static_pages/home'
+    page.should have_content('Sample App')
   end
 end
