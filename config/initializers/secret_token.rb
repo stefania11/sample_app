299a29d732bd98ac5809c6b6853a50e4ff29a3e2
Stefania Druga <druga.stefania@gmail.com>
2013-05-28 14:07:52 +0200

    remove hello world test

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,12 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key is used for verifying the integrity of signed cookies.
+# Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
-
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-SampleApp::Application.config.secret_key_base = '9b37a38cb115601b0bdbd3a2285e0234149dcf92c6f6925af6ab54fcba0bb4aa8f4139a7d15a12ec7a826826fcd8f784cf3186cae0a8c795b3a8013d8ccf6ae3'
+SampleApp::Application.config.secret_token = '7cb6e4ad8ba367435b1e24343fd0cb0800d3325901c23e32934b6a957157013607c1ad8f7bf9296116042cfea00e76c9199ca5cf26221e89ff87fe8ce0e7fc2a'
